1b3572d57675f9d9cb7b3a2b6f5bdf127aa98052
SmithAnthony <SmithAnthony@LAPTOP-SRRKON0V>
2021-06-13 22:50:35 -0500

    Modificacion

--- proyecto-web/src/com/graphics/proyecto/ChartView.java
+++ proyecto-web/src/com/graphics/proyecto/ChartView.java
@@ -26,11 +26,12 @@ package com.graphics.proyecto;
 import org.primefaces.event.ItemSelectEvent;
 import org.primefaces.model.chart.*;
 
-import javax.annotation.PostConstruct;
-import javax.enterprise.context.RequestScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-import javax.inject.Named;
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
