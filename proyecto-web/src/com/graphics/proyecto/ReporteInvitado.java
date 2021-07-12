778e827b72434599bddeeadd65456551306e4da9
alexis <nicolas_villita21@hotmail.es>
2021-07-11 22:31:59 -0500

    Ordenar valores reporte invitado

--- proyecto-web/src/com/graphics/proyecto/ReporteInvitado.java
+++ proyecto-web/src/com/graphics/proyecto/ReporteInvitado.java
@@ -2,6 +2,9 @@ package com.graphics.proyecto;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Vector;
 
@@ -27,16 +30,16 @@ public class ReporteInvitado implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	@EJB
 	private PaisDao paisDao;
-	
-	private List<Vector<Object>> prueba;
+
+	private List<Vector<Object>> paisValor;
 	private HorizontalBarChartModel grafico;
-	
-	public List<Vector<Object>> getPaises(){
+
+	public List<Vector<Object>> getPaises() {
 		List<Pais> paises = paisDao.todos();
-		prueba = new ArrayList<Vector<Object>>();
+		paisValor = new ArrayList<Vector<Object>>();
 		for (Pais p : paises) {
 			Vector<Object> datos = new Vector<Object>();
 			for (ValorSocial v : p.getSociales()) {
@@ -46,53 +49,102 @@ public class ReporteInvitado implements Serializable {
 				}
 				break;
 			}
-			prueba.add(datos);
+			paisValor.add(datos);
 		}
+		ordenar();
 		horizontalP();
-		return prueba;
+		return paisValor;
 	}
 	
+	private void ordenar() {
+		Vector<Object> aux;
+		for(int i = 2; i < paisValor.size(); i++) {
+			for(int j = 0; j < paisValor.size() - i; j++) {
+				if(Double.parseDouble(paisValor.get(j).get(1).toString()) < Double.parseDouble(paisValor.get(j+1).get(1).toString())) {
+					aux = paisValor.get(j);
+					paisValor.set(j, paisValor.get(j+1));
+					paisValor.set(j+1,aux);
+				}
+			}
+		}
+	}
+
+//	private List<Vector<Object>> burbuja(List<Vector<Object>>) {
+//		int auxiliar;
+//		int[] arregloOrdenado;
+//		for (int i = 2; i < arreglo.length; i++) {
+//			for (int j = 0; j < arreglo.length - i; j++) {
+//				if (arreglo[j] > arreglo[j + 1]) {
+//					auxiliar = arreglo[j];
+//					arreglo[j] = arreglo[j + 1];
+//					arreglo[j + 1] = auxiliar;
+//				}
+//			}
+//		}
+//		arregloOrdenado = arreglo;
+//		return arregloOrdenado;
+//	}
 	
+//	public static int[] burbuja(int[] arreglo)
+//    {
+//      int auxiliar;
+//      int[] arregloOrdenado;
+//      for(int i = 2; i < arreglo.length; i++)
+//      {
+//        for(int j = 0;j < arreglo.length-i;j++)
+//        {
+//          if(arreglo[j] > arreglo[j+1])
+//          {
+//            auxiliar = arreglo[j];
+//            arreglo[j] = arreglo[j+1];
+//            arreglo[j+1] = auxiliar;
+//          }   
+//        }
+//      }
+//      arregloOrdenado = arreglo;
+//      return arregloOrdenado;
+//    }
+	
+
 	public HorizontalBarChartModel getGrafico() {
 		return grafico;
 	}
-	
+
 	public void setGrafico(HorizontalBarChartModel grafico) {
 		this.grafico = grafico;
 	}
 
-
 	private void horizontalP() {
-        grafico = new HorizontalBarChartModel();
- 
-        ChartSeries paises = new ChartSeries();
-        paises.setLabel("Area(sq km)");
-        
-        for (int i=0;i<10;i++) {
-        	paises.set(prueba.get(i).get(0), Double.parseDouble(prueba.get(i).get(1).toString()));
-        }
+		grafico = new HorizontalBarChartModel();
+
+		ChartSeries paises = new ChartSeries();
+		paises.setLabel("Area(sq km)");
+
+		for (int i = 0; i < 10; i++) {
+			paises.set(paisValor.get(i).get(0), Double.parseDouble(paisValor.get(i).get(1).toString()));
+		}
 //        for(Vector<Object> pais : prueba) {
 //        	paises.set(pais.get(0), Double.parseDouble(pais.get(1).toString()));
 //        }
-        
-        grafico.addSeries(paises);
- 
-        grafico.setTitle("Area territorial");
-        grafico.setLegendPosition("e");
-        grafico.setStacked(true);
- 
-        Axis xAxis = grafico.getAxis(AxisType.X);
-        xAxis.setLabel("Extension");
-        xAxis.setMin(0);
-        xAxis.setMax(3000000);
- 
-        Axis yAxis = grafico.getAxis(AxisType.Y);
-        yAxis.setLabel("Pais");
-    }
-	
-	@PostConstruct
-	public void init() {
-		getPaises();
+
+		grafico.addSeries(paises);
+
+		grafico.setTitle("Area territorial");
+		grafico.setLegendPosition("e");
+		grafico.setStacked(true);
+
+		Axis xAxis = grafico.getAxis(AxisType.X);
+		xAxis.setLabel("Extension");
+		xAxis.setMin(0);
+		xAxis.setMax(3000000);
+
+		Axis yAxis = grafico.getAxis(AxisType.Y);
+		yAxis.setLabel("Pais");
 	}
+
+//	@PostConstruct
+//	public void init() {
+//		getPaises();
+//	}
 
 }
